9deee8e6b7f0fcdd95427fb8aca23a0fb8cb0a33
MELİSA ECE SURAL <47081681+MelisaEceSural@users.noreply.github.com>
2019-07-19 14:01:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using n11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            class1 dene = new class1();
+
+            dene.start();
+            dene.login();
+            dene.SearchBilgisayar();
+            dene.Paging();
+            dene.SepeteAt();
+            dene.finish();
+        }
+    }
+}
